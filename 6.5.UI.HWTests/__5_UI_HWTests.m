ad59b1a82f6c65c681bf419bfd92a1b8829462aa
Hua Jiang <rectinajh@163.com>
2015-06-05 17:17:53 +0800

    Initial Commit

---
+++ 6.5.UI.HWTests/__5_UI_HWTests.m
@@ -0,0 +1,40 @@
+//
+//  __5_UI_HWTests.m
+//  6.5.UI.HWTests
+//
+//  Created by rimi on 15/6/5.
+//  Copyright (c) 2015年 rectinajh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface __5_UI_HWTests : XCTestCase
+
+@end
+
+@implementation __5_UI_HWTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
